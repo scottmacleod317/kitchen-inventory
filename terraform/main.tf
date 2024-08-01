065930af95aed300bf54493d661a40e2c67faa9b
Scott MacLeod <scottmacleod317@gmail.com>
2024-08-01 14:09:48 +0100

    fix: Deploy app as cloud run service instead of job

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,48 @@
-resource "google_cloud_run_v2_job" "default" {
-  name     = "kitchen-inventory"
+resource "google_cloud_run_v2_service" "default" {
+  name     = "kitchen-inventory-service"
   location = var.region
+  ingress = "INGRESS_TRAFFIC_ALL"
 
   template {
-    template{
-      volumes {
-        name = "cloudsql"
-        cloud_sql_instance {
-          instances = [google_sql_database_instance.instance.connection_name]
-        }
-      }
+    scaling {
+      max_instance_count = 2
+    }
 
-      containers {
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [google_sql_database_instance.instance.connection_name]
+      }
+    }
+
+    containers {
         image = "europe-west2-docker.pkg.dev/solar-router-429817-i6/kitchen-inventory/kitchen-inv-app"
 
-        env {
-          name = "FOO"
-          value = "bar"
-        }
-        env {
-          name = "latestdclsecret"
-          value_source {
-            secret_key_ref {
-              secret = google_secret_manager_secret.secret.secret_id
-              version = "1"
-            }
+      env {
+        name = "FOO"
+        value = "bar"
+      }
+      env {
+        name = "SECRET_ENV_VAR"
+        value_source {
+          secret_key_ref {
+            secret = google_secret_manager_secret.secret.secret_id
+            version = "1"
           }
         }
-        volume_mounts {
-          name = "cloudsql"
-          mount_path = "/cloudsql"
-        }
+      }
+      volume_mounts {
+        name = "cloudsql"
+        mount_path = "/cloudsql"
       }
     }
   }
+
+  traffic {
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+  depends_on = [google_secret_manager_secret_version.secret-version-data]
 }
 
 data "google_project" "project" {
