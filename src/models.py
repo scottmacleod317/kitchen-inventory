2d518c46ef71b0212c1c9338e58429f3799bc7f3
Scott MacLeod <scottmacleod317@gmail.com>
2024-08-05 17:38:27 +0100

    fix: Add name column to product model

--- src/models.py
+++ src/models.py
@@ -8,6 +8,7 @@ class Product(Base):
 
     product_id = Column(Integer, primary_key=True, index=True)
     time_created = Column(DateTime, index=True, nullable=False)
+    name = Column(String, nullable=False)
     quantity = Column(Float, nullable=False)
     description = Column(String(30), nullable=True)
     category = Column(String(30), nullable=True)
