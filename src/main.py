37c8d0c0fd58213e65bb042f74d9892768dfbbdb
Scott MacLeod <scottmacleod317@gmail.com>
2024-08-07 10:23:32 +0100

    feat: Add endpoint for deleting a product

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
 from . import models, schemas, crud
@@ -29,10 +29,16 @@ def read_products(skip: int = 0, limit: int = 10, db: Session = Depends(get_db))
 def read_product(product_id: int, db: Session = Depends(get_db)):
     product = crud.get_product_by_id(db, product_id)
     if product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=404, detail="Product not found with ID {product_id}")
     return product
 
 
 @app.post("/product/", response_model=schemas.Product)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
     return crud.create_product(db=db, product=product)
+
+
+@app.delete("/product/{product_id}", response_model=schemas.Product)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    crud.delete_product_by_id(db, product_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
