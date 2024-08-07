3755e6658aab64f0269d58f9998957fda5aef9fd
scottmacleod317 <scottmacleod317@gmail.com>
2024-08-07 10:10:55 +0100

    feat: Get single product by ID (#12)

--- src/crud.py
+++ src/crud.py
@@ -10,8 +10,12 @@ def get_products(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Product).order_by(desc("time_created")).offset(skip).limit(limit).all()
 
 
+def get_product_by_id(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.product_id == product_id).first()
+
+
 def create_product(db: Session, product: schemas.ProductCreate):
-    new_product = models.Product(time_created=datetime.now(tz=timezone.utc), **product.dict())
+    new_product = models.Product(time_created=datetime.now(tz=timezone.utc), **product.model_dump())
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from . import models, schemas, crud
@@ -23,6 +23,14 @@ def get_db():
 @app.get("/products/", response_model=List[schemas.Product])
 def read_products(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_products(db, skip=skip, limit=limit)
+
+
+@app.get("/product/{product_id}", response_model=schemas.Product)
+def read_product(product_id: int, db: Session = Depends(get_db)):
+    product = crud.get_product_by_id(db, product_id)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
 
 
 @app.post("/product/", response_model=schemas.Product)
